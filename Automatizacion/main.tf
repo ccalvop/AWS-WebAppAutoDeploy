3c5de58ea08e96be7e02449e6abdda0aecf8ebcd
Carlos <126183973+ccalvop@users.noreply.github.com>
2023-04-20 09:26:41 +0200

    Update main.tf

--- Automatizacion/main.tf
+++ Automatizacion/main.tf
@@ -2,34 +2,53 @@ resource "aws_iam_user" "webapp_auto_deploy_user" {
   name = "WebAppAutoDeployUser"
 }
 
-resource "aws_iam_user_policy" "webapp_auto_deploy_policy" {
-  name = "WebAppAutoDeployPolicy"
-  user = aws_iam_user.webapp_auto_deploy_user.name
+resource "aws_iam_policy" "webapp_auto_deploy_policy" {
+  name        = "WebAppAutoDeployPolicy"
+  description = "Policy for WebAppAutoDeploy user"
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
-        Action = [
-          "s3:*"
-        ]
+        Effect   = "Allow"
+        Action   = "s3:*"
         Resource = "*"
       },
       {
-        Effect = "Allow"
-        Action = [
-          "codebuild:*"
-        ]
+        Effect   = "Allow"
+        Action   = "codebuild:*"
         Resource = "*"
       },
       {
-        Effect = "Allow"
-        Action = [
-          "codepipeline:*"
-        ]
+        Effect   = "Allow"
+        Action   = "codepipeline:*"
         Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = "codestar-connections:*"
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "iam:CreateRole",
+          "iam:AttachRolePolicy",
+          "iam:PutRolePolicy",
+          "iam:PassRole",
+        ]
+        Resource = "arn:aws:iam::*:role/service-role/*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = "iam:CreatePolicy"
+        Resource = "arn:aws:iam::*:policy/*"
       }
     ]
   })
+}
+
+resource "aws_iam_user_policy_attachment" "webapp_auto_deploy_user_policy_attachment" {
+  user       = aws_iam_user.webapp_auto_deploy_user.name
+  policy_arn = aws_iam_policy.webapp_auto_deploy_policy.arn
 }
